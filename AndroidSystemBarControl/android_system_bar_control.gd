d5f6d22c2c8d35fa4dfda9d5cda1e3b86806c70e
Amanullah Menjli <menjliamanullah@egg.tn>
2025-05-27 16:28:20 +0100

    feat: implement system bars transparency and light/dark mode toggles
    Added functions to allow easily toggling transparency for system bars
    and setting the wanted theme mode (dark/light).

---
+++ AndroidSystemBarControl/android_system_bar_control.gd
@@ -0,0 +1,56 @@
+@tool
+extends Node
+
+# needed Android utilities
+var android_runtime: JNISingleton
+var window: JavaObject
+var activity: JavaObject
+var layout_params: JavaClass
+var view: JavaClass
+var insets_controller: JavaObject
+var window_insets_controller: JavaClass
+
+func _ready() -> void:
+	if Engine.has_singleton("AndroidRuntime"):
+		android_runtime = Engine.get_singleton("AndroidRuntime")
+		activity = android_runtime.getActivity()
+		window = activity.getWindow()
+		layout_params = JavaClassWrapper.wrap("android.view.WindowManager$LayoutParams")
+		view = JavaClassWrapper.wrap("android.view.View")
+		insets_controller = window.getInsetsController()
+		window_insets_controller = JavaClassWrapper.wrap("android.view.WindowInsetsController")
+		window.addFlags(layout_params.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+	
+func toggle_system_bars_transparency(transparent: bool, dark: bool = false) -> void:
+	if transparent:
+		activity.runOnUiThread(android_runtime.createRunnableFromGodotCallable(make_system_bars_transparent))
+	else:
+		activity.runOnUiThread(android_runtime.createRunnableFromGodotCallable(make_system_bars_opaque))
+	
+	if dark:
+		insets_controller.setSystemBarsAppearance(
+				0,
+				window_insets_controller.APPEARANCE_LIGHT_STATUS_BARS
+		)
+	else:
+		insets_controller.setSystemBarsAppearance(
+				window_insets_controller.APPEARANCE_LIGHT_STATUS_BARS,
+				window_insets_controller.APPEARANCE_LIGHT_STATUS_BARS
+		)
+
+
+func make_system_bars_transparent() -> void:
+	# Allow UI to render behind status bar
+	window.getDecorView().setSystemUiVisibility(view.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+
+	# Make the system bars transparent
+	window.setStatusBarColor(Color.TRANSPARENT.to_argb32())
+	window.setNavigationBarColor(Color.TRANSPARENT.to_argb32())
+
+func make_system_bars_opaque() -> void:
+	# Allow UI to render behind status bar
+	window.getDecorView().setSystemUiVisibility(view.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+
+	# Make the system bars transparent
+	window.setStatusBarColor(Color.TRANSPARENT.to_argb32())
+	window.setNavigationBarColor(Color.TRANSPARENT.to_argb32())
--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Android System Bar Control
-A plugin to customize android's system bars (status and navigation bars).
+A plugin for Godot to customize android's system bars (status and navigation bars).
 
 ## Current features
 * Make the system bars transparent and render the UI bellow them.
+* Toggle dark/light mode for status bar (navigation bar changes dynamically based on what's bellow it)
+
+## How to use
+After adding the addon to your project and enabling it you can:
+### Enable transparency:
+```AndroidSystemBarControl.toggle_system_bars_transparency(true, true) # in dark mode (white icons)```
+```AndroidSystemBarControl.toggle_system_bars_transparency(true, false) # in light mode (black icons)```
+
+### Disable transparency
+```AndroidSystemBarControl.toggle_system_bars_transparency(false, true) # in dark mode (white icons)```
+```AndroidSystemBarControl.toggle_system_bars_transparency(false, false) # in light mode (black icons)```
